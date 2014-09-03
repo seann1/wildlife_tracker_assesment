247edde1d10099f21f5a0e7a221ca86e37a46667
Sean <seanniesen@gmail.com>
2014-09-03 05:11:51 +0000

    add create method

--- app/controllers/species_controller.rb
+++ app/controllers/species_controller.rb
@@ -8,4 +8,21 @@ class SpeciesController < ApplicationController
     @species = Species.new
     render('species/new.html.erb')
   end
+  
+  def show
+    @species = Species.find(params[:id])
+    render('species/show.html.erb')
+  end
+  
+  def create
+    @species = Species.new(:name => params[:name])
+
+    if @species.save
+      flash[:notice] = "Your species was saved"
+      render('species/show.html.erb')
+    else
+      flash[:notice] = "Your entry was not valid"
+      render('species/new.html.erb')
+    end
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
   match('/', {:via => :get, :to => 'species#index'})
   match('species/new', {:via => :get, :to => 'species#new'})
+  match('species/new', {:via => :post, :to => 'species#create'})
+  match('species/:id', {:via => :get, :to => 'species#show'})
 end
