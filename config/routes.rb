f8c238243f6c7a00a8ac1957114464c3ba6e27e9
Sean <seanniesen@gmail.com>
2014-09-05 05:31:26 +0000

    add index, add and create routes for sightings

--- config/routes.rb
+++ config/routes.rb
@@ -6,4 +6,8 @@ Rails.application.routes.draw do
   match('species/:id/edit', {:via => :get, :to => 'species#edit'})
   match('species/:id', {:via => [:patch, :put], :to => 'species#update'})
   match('species/:id', {:via => :delete, :to => 'species#destroy'})
+  match('sightings/index', {:via => :get, :to => 'sightings#index'})
+  match('sightings', {:via => :get, :to => 'sightings#index'})
+  match('species/:id/sightings', {:via => :post, :to => 'sightings#create'})
+  match('species/:id/sightings/new', {:via => :get, :to => 'sightings#new'})
 end
