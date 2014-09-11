1bb73e794bc7b654ed9bc4c3f3327497a0764707
Sean <student@epicodus.com>
2014-09-11 16:07:46 -0700

    fix species id targeting bug

--- app/controllers/species_controller.rb
+++ app/controllers/species_controller.rb
@@ -3,17 +3,17 @@ class SpeciesController < ApplicationController
     @species = Species.all
     render('species/index.html.erb')
   end
-  
-  def new
-    @species = Species.new
-    render('species/new.html.erb')
-  end
-  
+
   def show
     @species = Species.find(params[:id])
     render('species/show.html.erb')
   end
-  
+
+  def new
+    @species = Species.new
+    render('species/new.html.erb')
+  end
+
   def create
     @species = Species.new(params[:species])
 
@@ -25,12 +25,12 @@ class SpeciesController < ApplicationController
       render('species/new.html.erb')
     end
   end
-  
+
   def edit
     @species = Species.find(params[:id])
     render('species/edit.html.erb')
   end
-  
+
   def update
     @species = Species.find(params[:id])
     if @species.update(params[:species])
@@ -39,7 +39,7 @@ class SpeciesController < ApplicationController
       render('species/edit.html.erb')
     end
   end
-  
+
   def destroy
     @species = Species.find(params[:id])
     @species.destroy
